909a59462b2dca3872ad831f726bab8d69af4d70
rijan <hrijon.chp@gmail.com>
2020-03-05 09:47:20 +0800

    JS function goes out of scope

---
+++ lab02/q1a.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+function rainbow()
+ {
+	var colour;
+	colour="Red";
+
+
+console.log("colour");
+
+}
+
+
---
+++ lab02/q1b.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+function rainbow()
+ {
+	var colour;
+	colour="Red";
+
+
+console.log("colour");
+
+}
+
+rainbow();
+console.log("colour");
+
